7cb3c5b07ca9ff0c14051ab67563da090b6c0b1a
hwubh <www.180615938@qq.com>
2022-02-26 20:30:29 +0800

    ok

--- ImpSkelS20/CircleBrush.cpp
+++ ImpSkelS20/CircleBrush.cpp
@@ -1,3 +1,10 @@
+//
+// PointBrush.cpp
+//
+// The implementation of Point Brush. It is a kind of ImpBrush. All your brush implementations
+// will look like the file with the different GL primitive calls.
+//
+
 #include "impressionistDoc.h"
 #include "impressionistUI.h"
 #include "CircleBrush.h"
